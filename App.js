fe6fa6828354b2337eca833c913cb648781286cb
Belka-S <belka.srg@gmail.com>
2023-06-01 08:33:48 +0200

    add hw-1

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+ StyleSheet,
+ Text,
+ View,
+} from 'react-native';
+
+export default function App() {
+ return (
+  <View style={styles.container}>
+   <Text>
+    Open up App.js to start working on your app!
+    The first step.
+   </Text>
+   <StatusBar style='auto' />
+  </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+  flex: 1,
+  backgroundColor: 'white',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '26px',
+ },
+});
