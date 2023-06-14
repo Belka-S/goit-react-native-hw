ae47b52218b7a20e11e847ac068951e9a7349e77
Belka-S <belka.srg@gmail.com>
2023-06-14 14:01:21 +0200

    turn off React Native Debagger

--- src/services/translation.js
+++ src/services/translation.js
@@ -2,7 +2,6 @@ import translate from 'translate';
 import { getLocales } from 'expo-localization';
 
 const deviceLanguage = getLocales()[0]?.languageCode;
-console.log(getLocales()[0]);
 
 // Translation
 translate.engine = 'google'; // "google", "yandex", "libre", "deepl"
