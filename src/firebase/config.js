caaac6bf284d715ae82e50e9092015c434f298b6
Belka-S <belka.srg@gmail.com>
2023-06-18 08:41:19 +0200

    fix fb

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,23 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCx0as6NGuS0-8NSOLj_-6C8JldPazUbR8',
-//   authDomain: 'rn-image-hw.firebaseapp.com',
-//   projectId: 'rn-image-hw',
-//   storageBucket: 'rn-image-hw.appspot.com',
-//   messagingSenderId: '960606314061',
-//   appId: '1:960606314061:web:9f72e570aae89b12b9c604',
-//   measurementId: 'G-HKTSNRM3F0',
-// };
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyBX_ZtbZRvjFyU7mqn7i0Mj5EafDcOuKkY',
-  authDomain: 'rn-image-hw-fa53a.firebaseapp.com',
-  projectId: 'rn-image-hw-fa53a',
-  storageBucket: 'rn-image-hw-fa53a.appspot.com',
-  messagingSenderId: '882704976085',
-  appId: '1:882704976085:web:7ba22f8d837e2b607e3a5c',
+  apiKey: 'apiKey',
+  authDomain: 'authDomain',
+  projectId: 'projectId',
+  storageBucket: 'storageBucket',
+  messagingSenderId: 'messagingSenderId',
+  appId: 'appId',
 };
 
 // Initialize Firebase
